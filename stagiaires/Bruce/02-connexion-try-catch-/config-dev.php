aac6d83de8777bf163bf54c9b7cd84a23ea6b41a
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-04-27 09:28:34 +0200

    correction de lex 2

--- stagiaires/Bruce/02-connexion-try-catch-/config-dev.php
+++ stagiaires/Bruce/02-connexion-try-catch-/config-dev.php
@@ -1,11 +1,11 @@
 <?php
-//variable de production a dupliquer 
-//son le nom de config.php
 
-const DB_connect_type = "mysql";
-const DB_connect_host = "localhost";
-const DB_connect_port = "3307";
-const DB_connect_name = "listepays";
-const DB_connect_username = "root";
-const DB_connect_password = "";
-const DB_connect_charset = "utf8mb4";
+// variable de développement, à dupliquer
+// son le nom de config.php
+const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
+const DB_CONNECT_HOST = "localhost";
+const DB_CONNECT_PORT = 3307;// WAMP -> MariaDB
+const DB_CONNECT_NAME = "listepays";
+const DB_CONNECT_CHARSET = "utf8mb4";
+const DB_CONNECT_USER = "root";
+const DB_CONNECT_PWD = "";
